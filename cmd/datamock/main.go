c0a971b87b79cf735cd72c74dc90ec911b187bde
xuhe <2635484233@qq.com>
2026-05-10 11:45:26 +0800

    feat: add --port arg

--- cmd/datamock/main.go
+++ cmd/datamock/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"embed"
 	"errors"
+	"flag"
 	"fmt"
 	"io/fs"
 	"log"
@@ -14,10 +15,17 @@ import (
 	"strings"
 )
 
+const defaultPort = 5179
+
 //go:embed web/dist
 var embeddedFiles embed.FS
 
 func main() {
+	host := flag.String("host", "127.0.0.1", "host address to listen on")
+	port := flag.Int("port", defaultPort, "port to listen on, use 0 for a random available port")
+	noOpen := flag.Bool("no-open", false, "do not open the browser automatically")
+	flag.Parse()
+
 	if err := mime.AddExtensionType(".js", "text/javascript; charset=utf-8"); err != nil {
 		log.Printf("register js mime type: %v", err)
 	}
@@ -27,7 +35,7 @@ func main() {
 		log.Fatalf("load embedded web assets: %v", err)
 	}
 
-	listener, err := net.Listen("tcp", "127.0.0.1:0")
+	listener, err := listen(*host, *port)
 	if err != nil {
 		log.Fatalf("start listener: %v", err)
 	}
@@ -39,15 +47,32 @@ func main() {
 	url := fmt.Sprintf("http://%s", listener.Addr().String())
 	fmt.Printf("DataMock is running at %s\n", url)
 
-	go func() {
-		if err := openBrowser(url); err != nil {
-			log.Printf("open browser: %v", err)
-		}
-	}()
+	if !*noOpen {
+		go func() {
+			if err := openBrowser(url); err != nil {
+				log.Printf("open browser: %v", err)
+			}
+		}()
+	}
 
 	if err := http.Serve(listener, mux); err != nil && !errors.Is(err, http.ErrServerClosed) {
 		log.Fatalf("serve: %v", err)
 	}
+}
+
+func listen(host string, port int) (net.Listener, error) {
+	address := fmt.Sprintf("%s:%d", host, port)
+	listener, err := net.Listen("tcp", address)
+	if err == nil {
+		return listener, nil
+	}
+
+	if port == 0 {
+		return nil, err
+	}
+
+	log.Printf("%s is unavailable, falling back to a random port: %v", address, err)
+	return net.Listen("tcp", fmt.Sprintf("%s:0", host))
 }
 
 func spaHandler(files fs.FS) http.Handler {
